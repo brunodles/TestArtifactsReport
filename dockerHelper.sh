2f8d277788bc4d9ec7c387cee9b7711c703ef655
Bruno de Lima e Silva <bruno.silva@concrete.com.br>
2018-02-15 15:34:47 -0200

    Update refactor to funnctions

--- dockerHelper.sh
+++ dockerHelper.sh
@@ -5,74 +5,41 @@ readonly CONTAINER_NAME=${IMAGE_NAME%%:*}_${PWD##*/}
 #readonly CONTAINER_NAME=testArtifactsAnalyzer
 readonly PORTS=3000:5000
 
-case $1 in
-  create)
-    if [ "$DEFAULT_IMAGE" == "true" ]; then
-      echo "No need to create, using default node image"
-    else
-    docker image build -t $IMAGE_NAME . -f-<<EOF
-FROM node:9.4
-
-USER node
-RUN mkdir /home/node/.npm-global
-ENV PATH=/home/node/.npm-global/bin:$PATH
-ENV NPM_CONFIG_PREFIX=/home/node/.npm-global
-
-RUN npm install -g create-react-app
-USER root
-EOF
-    fi
-  ;;
-  run)
-    (
-      docker run -it -v "$(pwd):/home/node/app" -w "/home/node/app" --user=node -p $PORTS --name $CONTAINER_NAME $IMAGE_NAME ${@:2}
-    ) || (
-      echo "Did it failed? Try \"$0 restart\""
-    )
-  ;;
-  restart)
-    echo "Restarting"
-    docker restart $CONTAINER_NAME
-    $0 exec
-  ;;
-  exec)
-    (
-      docker exec -it $CONTAINER_NAME ${@:2}
-    ) || (
-      echo "Did it failed? Try \"$0 bash\""
-    )
-  ;;
-  rootExec)
-    (
-      docker exec -u 0 -it $CONTAINER_NAME ${@:2}
-    ) || (
-      echo "Error"
-    )
-  ;;
-  stop)
-    echo "Stopping"
-    docker stop --time 30 $CONTAINER_NAME
-  ;;
-  kill)
-    echo "Killing"
-    docker stop $CONTAINER_NAME
-  ;;
-  rm)
-    echo "Removing container"
-    docker rm $CONTAINER_NAME
-  ;;
-  rmi)
-    echo "Removing image"
-    docker rmi $IMAGE_NAME
-  ;;
-  vars)
-    echo DEFAULT_IMAGE=$DEFAULT_IMAGE
-    echo IMAGE_NAME=$IMAGE_NAME
-    echo CONTAINER_NAME=$CONTAINER_NAME
-    echo PORTS=$PORTS
-  ;;
-  help|--help|-h)
-    cat <<-TEXT
+command=$([[ -z "${@:2}" ]] && echo "bash" || echo ${@:2})
+main() {
+  case $1 in
+    create)
+      create
+    ;;
+    run)
+      run
+    ;;
+    restart)
+      restart
+    ;;
+    exec)
+      exec
+    ;;
+    rootExec)
+      rootExec
+    ;;
+    stop)
+      stop
+    ;;
+    kill)
+      kill
+    ;;
+    rm)
+      rm
+    ;;
+    rmi)
+      rmi
+    ;;
+    var|vars)
+      vars
+    ;;
+    help|--help|-h)
+      cat <<-TEXT
 Docker Helper
 Useful scripts to work with docker.
 
@@ -90,9 +57,88 @@ Options:
   vars      		  Show vars
   help      		  Show this help
 TEXT
-    ;;
-    *)
-      echo "Option \"$1\" not found!"
-      $0 help
-    ;;
-esac
+      ;;
+      *)
+        echo "Option \"$1\" not found!"
+        $0 help
+      ;;
+  esac
+}
+
+create() {
+  if [ "$DEFAULT_IMAGE" == "true" ]; then
+    echo "No need to create, using default node image"
+  else
+  docker image build -t $IMAGE_NAME . -f-<<EOF
+FROM node:9.4
+
+USER node
+RUN mkdir /home/node/.npm-global
+ENV PATH=/home/node/.npm-global/bin:$PATH
+ENV NPM_CONFIG_PREFIX=/home/node/.npm-global
+
+RUN npm install -g create-react-app
+USER root
+EOF
+  fi
+}
+
+run() {
+  (
+    docker run -it -v "$(pwd):/home/node/app" -w "/home/node/app" --user=node -p $PORTS --name $CONTAINER_NAME $IMAGE_NAME $command
+  ) || (
+    echo "Did it failed? Try \"$0 restart\""
+  )
+}
+
+restart() {
+  echo "Restarting"
+  docker restart $CONTAINER_NAME
+  exec $command
+}
+
+exec() {
+  (
+    docker exec -it $CONTAINER_NAME $command
+  ) || (
+    echo "Did it failed? Try \"$0 bash\""
+  )
+}
+
+rootExec() {
+  (
+    docker exec -u 0 -it $CONTAINER_NAME ${@:2}
+  ) || (
+    echo "Error"
+  )
+}
+
+stop() {
+  echo "Stopping"
+  docker stop --time 30 $CONTAINER_NAME
+}
+
+kill() {
+  echo "Killing"
+  docker stop $CONTAINER_NAME
+}
+
+rm() {
+  echo "Removing container"
+  docker rm $CONTAINER_NAME
+}
+
+rmi() {
+  echo "Removing image"
+  docker rmi $IMAGE_NAME
+}
+
+vars() {
+  echo DEFAULT_IMAGE=$DEFAULT_IMAGE
+  echo IMAGE_NAME=$IMAGE_NAME
+  echo CONTAINER_NAME=$CONTAINER_NAME
+  echo PORTS=$PORTS
+  echo command=$command
+}
+
+main $@
